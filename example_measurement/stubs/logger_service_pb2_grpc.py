57901c34a73facbb3a8e06a8dc6fc2c2adcc1515
Aravind Murali <arvind.murali@emerson.com>
2025-04-09 18:37:19 +0530

    refactor: update session constructor

--- example_measurement/stubs/logger_service_pb2_grpc.py
+++ example_measurement/stubs/logger_service_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import logger_service_pb2 as logger__service__pb2
+import stubs.logger_service_pb2 as logger__service__pb2
 
 GRPC_GENERATED_VERSION = '1.72.0rc1'
 GRPC_VERSION = grpc.__version__
