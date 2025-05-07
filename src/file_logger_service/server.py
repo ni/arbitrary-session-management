aad3ae29e48d8aef4cad2d021e91103019c902e7
Aravind Murali <arvind.murali@emerson.com>
2025-05-07 22:34:06 +0530

    temp commit

--- src/file_logger_service/server.py
+++ src/file_logger_service/server.py
@@ -25,8 +25,8 @@ from file_logger_service.stubs.file_logger_service_pb2_grpc import (
 from ni_measurement_plugin_sdk_service.discovery import DiscoveryClient, ServiceLocation
 from ni_measurement_plugin_sdk_service.measurement.info import ServiceInfo
 
-GRPC_SERVICE_INTERFACE_NAME = "user.defined.file.v1.LogService"
-GRPC_SERVICE_CLASS = "user.defined.logger.v1.LogService"
+GRPC_SERVICE_INTERFACE_NAME = "user.defined.v1.CustomService"
+GRPC_SERVICE_CLASS = "user.defined.FileLogService"
 DISPLAY_NAME = "File Logger Service"
 
 
@@ -78,95 +78,6 @@ class FileLoggerServicer(FileLoggerServiceServicer):
             return handler(request.file_path, context)
 
         context.abort(grpc.StatusCode.INVALID_ARGUMENT, "Invalid initialization behavior.")
-
-    def _auto_initialize_session(
-        self,
-        file_path: str,
-        context: grpc.ServicerContext,
-    ) -> InitializeFileResponse:
-        """Initialize the session for UNSPECIFIED behavior of the initialization behavior.
-
-        If the session already exists and is open, it returns the existing session.
-        If the session does not exist or is closed, it creates a new session.
-
-        Args:
-            file_path: Path of the file to initialize the session.
-            context: gRPC context object for the request.
-
-        Returns:
-            InitializeResponse with session name and new session status.
-        """
-        session = self.sessions.get(file_path)
-
-        if session and not session.file_handle.closed:
-            return InitializeFileResponse(
-                session_name=session.session_name,
-                new_session=False,
-            )
-
-        return self._create_new_session(file_path, context)
-
-    def _create_new_session(
-        self,
-        file_path: str,
-        context: grpc.ServicerContext,
-    ) -> InitializeFileResponse:
-        """Create a new session.
-
-        If the session does not exist, it creates a new session.
-        Returns an ALREADY_EXISTS error if the session already exists and is open.
-
-        Args:
-            file_path: Path of the file to create a new session.
-            context: gRPC context object for the request.
-
-        Returns:
-            InitializeResponse with session name and new session status.
-        """
-        if file_path in self.sessions and not self.sessions[file_path].file_handle.closed:
-            context.abort(
-                grpc.StatusCode.ALREADY_EXISTS,
-                f"Session for '{file_path}' already exists and is open.",
-            )
-
-        file_handle: TextIO = open(file_path, "a+")
-        session_name: str = str(uuid.uuid4())
-        self.sessions[file_path] = Session(session_name=session_name, file_handle=file_handle)
-
-        return InitializeFileResponse(
-            session_name=session_name,
-            new_session=True,
-        )
-
-    def _attach_existing_session(  # type: ignore
-        self,
-        file_path: str,
-        context: grpc.ServicerContext,
-    ) -> InitializeFileResponse:
-        """Attach to the existing session.
-
-        Returns the existing session if it is open.
-        If the session does not exist or is closed, it returns NOT_FOUND error.
-
-        Args:
-            file_path: Path of the file to attach to the existing session.
-            context: gRPC context object for the request.
-
-        Returns:
-            InitializeResponse with session name and new session status.
-        """
-        session = self.sessions.get(file_path)
-
-        if session and not session.file_handle.closed:
-            return InitializeFileResponse(
-                session_name=session.session_name,
-                new_session=False,
-            )
-
-        context.abort(
-            grpc.StatusCode.NOT_FOUND,
-            f"Session for '{file_path}' does not exist or is closed.",
-        )
 
     def LogData(  # noqa: N802 - function name should be lowercase
         self,
@@ -244,6 +155,121 @@ class FileLoggerServicer(FileLoggerServiceServicer):
         session.file_handle.close()
         return CloseFileResponse()
 
+    def clean_up(self) -> None:
+        """Clean up all active file sessions."""
+        for session in self.sessions.values():
+            if not session.file_handle.closed:
+                session.file_handle.close()
+        self.sessions.clear()
+
+    def _auto_initialize_session(
+        self,
+        file_path: str,
+        context: grpc.ServicerContext,
+    ) -> InitializeFileResponse:
+        """Initialize the session for UNSPECIFIED behavior of the initialization behavior.
+
+        If the session already exists and is open, it returns the existing session.
+        If the session does not exist or is closed, it creates a new session.
+
+        Args:
+            file_path: Path of the file to initialize the session.
+            context: gRPC context object for the request.
+
+        Returns:
+            InitializeResponse with session name and new session status.
+        """
+        session = self.sessions.get(file_path)
+
+        if session and not session.file_handle.closed:
+            return InitializeFileResponse(
+                session_name=session.session_name,
+                new_session=False,
+            )
+
+        return self._create_new_session(file_path, context)
+
+    def _create_new_session(
+        self,
+        file_path: str,
+        context: grpc.ServicerContext,
+    ) -> InitializeFileResponse:
+        """Create a new session.
+
+        If the session does not exist, it creates a new session.
+        Returns an ALREADY_EXISTS error if the session already exists and is open.
+
+        Args:
+            file_path: Path of the file to create a new session.
+            context: gRPC context object for the request.
+
+        Returns:
+            InitializeResponse with session name and new session status.
+        """
+        if file_path in self.sessions and not self.sessions[file_path].file_handle.closed:
+            context.abort(
+                grpc.StatusCode.ALREADY_EXISTS,
+                f"Session for '{file_path}' already exists and is open.",
+            )
+
+        try:
+            file_handle: TextIO = open(file_path, "a+")
+        except FileNotFoundError:
+            context.abort(
+                grpc.StatusCode.NOT_FOUND, f"The specified path '{file_path}' does not exist."
+            )
+        except PermissionError:
+            context.abort(
+                grpc.StatusCode.PERMISSION_DENIED,
+                f"Permission denied while accessing '{file_path}'.",
+            )
+        except OSError as e:
+            context.abort(grpc.StatusCode.INTERNAL, f"Failed to open file '{file_path}': {e}")
+        
+        except Exception as e:
+            context.abort(
+                grpc.StatusCode.INTERNAL,
+                f"An error occurred while opening the file '{file_path}': {e}",
+            )
+
+        session_name: str = str(uuid.uuid4())
+        self.sessions[file_path] = Session(session_name=session_name, file_handle=file_handle)
+
+        return InitializeFileResponse(
+            session_name=session_name,
+            new_session=True,
+        )
+
+    def _attach_existing_session(  # type: ignore
+        self,
+        file_path: str,
+        context: grpc.ServicerContext,
+    ) -> InitializeFileResponse:
+        """Attach to the existing session.
+
+        Returns the existing session if it is open.
+        If the session does not exist or is closed, it returns NOT_FOUND error.
+
+        Args:
+            file_path: Path of the file to attach to the existing session.
+            context: gRPC context object for the request.
+
+        Returns:
+            InitializeResponse with session name and new session status.
+        """
+        session = self.sessions.get(file_path)
+
+        if session and not session.file_handle.closed:
+            return InitializeFileResponse(
+                session_name=session.session_name,
+                new_session=False,
+            )
+
+        context.abort(
+            grpc.StatusCode.NOT_FOUND,
+            f"Session for '{file_path}' does not exist or is closed.",
+        )
+
     def _get_session_by_name(
         self,
         session_name: str,
@@ -286,8 +312,9 @@ def start_server() -> None:
     logger = logging.getLogger(__name__)
     logger.info("Starting the File Logger Service...")
 
+    servicer = FileLoggerServicer()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_FileLoggerServiceServicer_to_server(FileLoggerServicer(), server)
+    add_FileLoggerServiceServicer_to_server(servicer, server)
     host = "[::1]"
     port = str(server.add_insecure_port(f"{host}:0"))
     server.start()
@@ -305,6 +332,7 @@ def start_server() -> None:
     logger.info(f"File Logger Service started on port {port}")
     input("Press Enter to stop the server.")
 
+    servicer.clean_up()
     discovery_client.unregister_service(registration_id)
     server.stop(grace=5)
     server.wait_for_termination()
