2a465fe7476424e54f33afade3b7675df5d26276
Aravind Murali <arvind.murali@emerson.com>
2025-05-19 12:09:54 +0530

    temp commit

---
+++ examples/nidcpower_measurement_with_logger/session_constructor.py
@@ -0,0 +1,40 @@
+"""File containing class for constructing file logger session."""
+
+from ni_measurement_plugin_sdk_service.session_management import (
+    SessionInformation,
+    SessionInitializationBehavior,
+)
+from session import FileLoggerServiceClient
+
+
+class FileLoggerSessionConstructor:
+    """Class for constructing a file logger session."""
+
+    def __init__(
+        self,
+        initialization_behavior: SessionInitializationBehavior = SessionInitializationBehavior.AUTO,
+    ) -> None:
+        """Initialize the FileLoggerSessionConstructor.
+
+        This class is used to construct a file logger session.
+
+        Args:
+            initialization_behavior: Initialization behavior for the file logger session.
+                Defaults to SessionInitializationBehavior.AUTO.
+        """
+        self.initialization_behavior = initialization_behavior
+
+    def __call__(self, session_info: SessionInformation) -> FileLoggerServiceClient:
+        """Call the FileLoggerServiceClient when this is called from a context manager.
+
+        Args:
+            session_info: The session information object containing the session name.
+
+        Returns:
+            The FileLoggerServiceClient object.
+        """
+        client = FileLoggerServiceClient(
+            file_path=session_info.resource_name,  # The resource name is the absolute file path.
+            initialization_behavior=self.initialization_behavior,
+        )
+        return client
--- examples/teststand_sequence/teststand_logger.py
+++ examples/teststand_sequence/teststand_logger.py
@@ -1,75 +1,15 @@
-"""Functions to set up and tear down sessions of NI-DMM devices and files in NI TestStand."""
-
 from typing import Any
 
-from _helpers import TestStandSupport
-from measurement import FILE_SERVICE_INSTRUMENT_TYPE
 from ni_measurement_plugin_sdk_service.discovery import DiscoveryClient
 from ni_measurement_plugin_sdk_service.grpc.channelpool import GrpcChannelPool
 from ni_measurement_plugin_sdk_service.session_management import (
-    INSTRUMENT_TYPE_NI_DMM,
     PinMapContext,
     SessionInitializationBehavior,
     SessionManagementClient,
 )
-from session_constructor import FileLoggerSessionConstructor
-
-
-def create_nidmm_sessions(sequence_context: Any) -> None:
-    """Create and register all NI-DMM sessions.
-
-    Args:
-        sequence_context: The SequenceContext COM object from the TestStand sequence execution.
-            (Dynamically typed.)
-    """
-    with GrpcChannelPool() as grpc_channel_pool:
-        teststand_support = TestStandSupport(sequence_context)
-        pin_map_id = teststand_support.get_active_pin_map_id()
-        pin_map_context = PinMapContext(pin_map_id=pin_map_id, sites=None)
-
-        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
-        session_management_client = SessionManagementClient(
-            discovery_client=discovery_client,
-            grpc_channel_pool=grpc_channel_pool,
-        )
-
-        # Reserve sessions for NI-DMM in NI Session Management Service.
-        with session_management_client.reserve_sessions(
-            pin_map_context,
-            instrument_type_id=INSTRUMENT_TYPE_NI_DMM,
-        ) as reservation:
-            # Initialize the sessions in NI gRPC Device Server.
-            with reservation.initialize_nidmm_sessions(
-                initialization_behavior=SessionInitializationBehavior.INITIALIZE_SESSION_THEN_DETACH
-            ):
-                pass
-            # Register the sessions in NI Session Management Service.
-            session_management_client.register_sessions(reservation.session_info)
-
-
-def destroy_nidmm_sessions() -> None:
-    """Destroy and unregister all NI-DMM sessions."""
-    with GrpcChannelPool() as grpc_channel_pool:
-        discovery_client = DiscoveryClient(grpc_channel_pool=grpc_channel_pool)
-        session_management_client = SessionManagementClient(
-            discovery_client=discovery_client,
-            grpc_channel_pool=grpc_channel_pool,
-        )
-
-        # Reserve all registered sessions for NI-DMM in NI Session Management Service.
-        with session_management_client.reserve_all_registered_sessions(
-            instrument_type_id=INSTRUMENT_TYPE_NI_DMM,
-        ) as reservation:
-            if not reservation.session_info:
-                return
-
-            session_management_client.unregister_sessions(reservation.session_info)
-
-            # Attach and close the sessions in NI gRPC Device Server.
-            with reservation.initialize_nidmm_sessions(
-                initialization_behavior=SessionInitializationBehavior.ATTACH_TO_SESSION_THEN_CLOSE
-            ):
-                pass
+from _helpers import TestStandSupport
+from nidmm_measurement_with_logger.measurement import FILE_SERVICE_INSTRUMENT_TYPE
+from nidmm_measurement_with_logger.session_constructor import FileLoggerSessionConstructor
 
 
 def create_file_sessions(sequence_context: Any) -> None:
