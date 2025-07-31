2926d9b94e911b0b7fad1097c8a5856a4cce9049
Giriharan219 <67152685+Giriharan219@users.noreply.github.com>
2025-07-31 15:07:58 +0530

    feat: Server-side implementation for device comm example (#18)
    What does this Pull Request accomplish?
    This pull request accomplishes the addition of Python gRPC server code for device communication based custom instrument example.
    
    Why should this Pull Request be merged?
    These changes enable the users to correlate with their use cases for better leverage of Arbitrary Session Management.
    
    What testing has been done?
    Manual testing.

---
+++ src/custom_instr_session_sharing/server/constants.py
@@ -0,0 +1,47 @@
+"""This module defines constants and enumerations for device communication."""
+
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum
+from typing import Dict
+
+from stubs.device_comm_service_pb2 import Protocol
+
+
+class GPIOPort(IntEnum):
+    """Enumeration for GPIO ports."""
+
+    PORT0 = 0
+    PORT1 = 1
+    PORT2 = 2
+    PORT3 = 3
+
+
+class GPIOChannel(IntEnum):
+    """Enumeration for GPIO channels."""
+
+    CH0 = 0
+    CH1 = 1
+    CH2 = 2
+    CH3 = 3
+    CH4 = 4
+    CH5 = 5
+    CH6 = 6
+    CH7 = 7
+
+
+class GPIOChannelState(Enum):
+    """GPIO channel states."""
+
+    LOW = False
+    HIGH = True
+
+
+@dataclass
+class Session:
+    """A session that contains device communication details."""
+
+    session_name: str
+    protocol: Protocol
+    register_map_path: str
+    register_data: Dict[str, int] = field(default_factory=dict)
+    reset: bool = False
