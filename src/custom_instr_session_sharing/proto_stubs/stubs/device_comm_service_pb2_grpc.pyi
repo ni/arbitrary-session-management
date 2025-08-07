6fbf26fe15750a6a45660b0cd97c530625199093
Giriharan Ravichandran <giriharan.ravichandran@ni.com>
2025-08-07 19:57:11 +0530

    Revert "fix: update device ID to custom instr resource name"
    This reverts commit de7c2f315d6f1bd3636a9caeddc1cd0d99eafceb.

--- src/custom_instr_session_sharing/proto_stubs/stubs/device_comm_service_pb2_grpc.pyi
+++ src/custom_instr_session_sharing/proto_stubs/stubs/device_comm_service_pb2_grpc.pyi
@@ -6,7 +6,7 @@ This file is where the request and response messages for the
 Device Communication service are defined. It includes the service definition, request and response
 messages, and enumerations for session initialization behavior.
 
-The Device Communication service provides methods to open a handle of a session.
+The Device Communication service provides methods to open a  handle of a session.
 
 The user can create a similar file for their services by following the structure of this file.
 It is recommended to have Initialize or similar rpc call 
@@ -42,7 +42,7 @@ class DeviceCommunicationStub:
     ]
     """Initializes the device communication session for DUT validation.
     Status Codes for errors:
-    - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path, reset or Invalid Session Initialization Behavior.
+    - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path or Invalid Session Initialization Behavior.
     - PERMISSION_DENIED: Permission denied for the register map path.
     - INTERNAL: Register Map path is invalid or inaccessible or any other unexpected behavior.
     - ALREADY_EXISTS: Device Session has already been initialized and cannot be initialized again for SESSION_INITIALIZATION_BEHAVIOR_INITIALIZE_NEW.
@@ -53,67 +53,37 @@ class DeviceCommunicationStub:
         device_comm_service_pb2.WriteRegisterRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to a specified register on the DUT.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for Register name
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to a specified register on the DUT."""
 
     ReadRegister: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadRegisterRequest,
         device_comm_service_pb2.ReadRegisterResponse,
     ]
-    """Reads a value from a specified register on the DUT.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for Register name
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads a value from a specified register on the DUT."""
 
     WriteGpioChannel: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.WriteGpioChannelRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to a specific GPIO channel
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO channel, state
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to a specific GPIO channel"""
 
     ReadGpioChannel: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadGpioChannelRequest,
         device_comm_service_pb2.ReadGpioChannelResponse,
     ]
-    """Reads the value of a specific GPIO channel
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO channel
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads the value of a specific GPIO channel"""
 
     WriteGpioPort: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.WriteGpioPortRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to an entire GPIO port.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO Port, State, Mask.
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to an entire GPIO port."""
 
     ReadGpioPort: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadGpioPortRequest,
         device_comm_service_pb2.ReadGpioPortResponse,
     ]
-    """Reads the value of an entire GPIO port.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO Port, Mask.
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads the value of an entire GPIO port."""
 
     Close: grpc.UnaryUnaryMultiCallable[
         device_comm_service_pb2.CloseRequest,
@@ -134,7 +104,7 @@ class DeviceCommunicationAsyncStub:
     ]
     """Initializes the device communication session for DUT validation.
     Status Codes for errors:
-    - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path, reset or Invalid Session Initialization Behavior.
+    - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path or Invalid Session Initialization Behavior.
     - PERMISSION_DENIED: Permission denied for the register map path.
     - INTERNAL: Register Map path is invalid or inaccessible or any other unexpected behavior.
     - ALREADY_EXISTS: Device Session has already been initialized and cannot be initialized again for SESSION_INITIALIZATION_BEHAVIOR_INITIALIZE_NEW.
@@ -145,67 +115,37 @@ class DeviceCommunicationAsyncStub:
         device_comm_service_pb2.WriteRegisterRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to a specified register on the DUT.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for Register name
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to a specified register on the DUT."""
 
     ReadRegister: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadRegisterRequest,
         device_comm_service_pb2.ReadRegisterResponse,
     ]
-    """Reads a value from a specified register on the DUT.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for Register name
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads a value from a specified register on the DUT."""
 
     WriteGpioChannel: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.WriteGpioChannelRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to a specific GPIO channel
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO channel, state
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to a specific GPIO channel"""
 
     ReadGpioChannel: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadGpioChannelRequest,
         device_comm_service_pb2.ReadGpioChannelResponse,
     ]
-    """Reads the value of a specific GPIO channel
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO channel
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads the value of a specific GPIO channel"""
 
     WriteGpioPort: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.WriteGpioPortRequest,
         device_comm_service_pb2.StatusResponse,
     ]
-    """Writes a value to an entire GPIO port.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO Port, State, Mask.
-    INTERNAL: Unexpected internal error.
-    """
+    """Writes a value to an entire GPIO port."""
 
     ReadGpioPort: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.ReadGpioPortRequest,
         device_comm_service_pb2.ReadGpioPortResponse,
     ]
-    """Reads the value of an entire GPIO port.
-    Status Codes for errors:
-    NOT_FOUND: Session does not exist.
-    INVALID_ARGUMENT: Invalid arguments for GPIO Port, Mask.
-    INTERNAL: Unexpected internal error.
-    """
+    """Reads the value of an entire GPIO port."""
 
     Close: grpc.aio.UnaryUnaryMultiCallable[
         device_comm_service_pb2.CloseRequest,
@@ -228,7 +168,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
     ) -> typing.Union[device_comm_service_pb2.InitializeResponse, collections.abc.Awaitable[device_comm_service_pb2.InitializeResponse]]:
         """Initializes the device communication session for DUT validation.
         Status Codes for errors:
-        - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path, reset or Invalid Session Initialization Behavior.
+        - INVALID_ARGUMENT: Invalid arguments for Device id, Protocol, Register map path or Invalid Session Initialization Behavior.
         - PERMISSION_DENIED: Permission denied for the register map path.
         - INTERNAL: Register Map path is invalid or inaccessible or any other unexpected behavior.
         - ALREADY_EXISTS: Device Session has already been initialized and cannot be initialized again for SESSION_INITIALIZATION_BEHAVIOR_INITIALIZE_NEW.
@@ -241,12 +181,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.WriteRegisterRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.StatusResponse, collections.abc.Awaitable[device_comm_service_pb2.StatusResponse]]:
-        """Writes a value to a specified register on the DUT.
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for Register name
-        INTERNAL: Unexpected internal error.
-        """
+        """Writes a value to a specified register on the DUT."""
 
     @abc.abstractmethod
     def ReadRegister(
@@ -254,12 +189,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.ReadRegisterRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.ReadRegisterResponse, collections.abc.Awaitable[device_comm_service_pb2.ReadRegisterResponse]]:
-        """Reads a value from a specified register on the DUT.
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for Register name
-        INTERNAL: Unexpected internal error.
-        """
+        """Reads a value from a specified register on the DUT."""
 
     @abc.abstractmethod
     def WriteGpioChannel(
@@ -267,12 +197,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.WriteGpioChannelRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.StatusResponse, collections.abc.Awaitable[device_comm_service_pb2.StatusResponse]]:
-        """Writes a value to a specific GPIO channel
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for GPIO channel, state
-        INTERNAL: Unexpected internal error.
-        """
+        """Writes a value to a specific GPIO channel"""
 
     @abc.abstractmethod
     def ReadGpioChannel(
@@ -280,12 +205,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.ReadGpioChannelRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.ReadGpioChannelResponse, collections.abc.Awaitable[device_comm_service_pb2.ReadGpioChannelResponse]]:
-        """Reads the value of a specific GPIO channel
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for GPIO channel
-        INTERNAL: Unexpected internal error.
-        """
+        """Reads the value of a specific GPIO channel"""
 
     @abc.abstractmethod
     def WriteGpioPort(
@@ -293,12 +213,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.WriteGpioPortRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.StatusResponse, collections.abc.Awaitable[device_comm_service_pb2.StatusResponse]]:
-        """Writes a value to an entire GPIO port.
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for GPIO Port, State, Mask.
-        INTERNAL: Unexpected internal error.
-        """
+        """Writes a value to an entire GPIO port."""
 
     @abc.abstractmethod
     def ReadGpioPort(
@@ -306,12 +221,7 @@ class DeviceCommunicationServicer(metaclass=abc.ABCMeta):
         request: device_comm_service_pb2.ReadGpioPortRequest,
         context: _ServicerContext,
     ) -> typing.Union[device_comm_service_pb2.ReadGpioPortResponse, collections.abc.Awaitable[device_comm_service_pb2.ReadGpioPortResponse]]:
-        """Reads the value of an entire GPIO port.
-        Status Codes for errors:
-        NOT_FOUND: Session does not exist.
-        INVALID_ARGUMENT: Invalid arguments for GPIO Port, Mask.
-        INTERNAL: Unexpected internal error.
-        """
+        """Reads the value of an entire GPIO port."""
 
     @abc.abstractmethod
     def Close(
