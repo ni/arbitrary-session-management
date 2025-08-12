686e89f8e232282f4c290b8b9d0f15c733f3643f
Giriharan Ravichandran <giriharan.ravichandran@ni.com>
2025-08-13 00:21:45 +0530

    feat:  update simple measurement example

--- src/custom_instr_session_sharing/examples/simple measurement/measurement.py
+++ src/custom_instr_session_sharing/examples/simple measurement/measurement.py
@@ -1,4 +1,4 @@
-"""A default measurement with an array in and out."""
+"""Simple Measurement Plug-In example to demonstrate the device communication using gRPC service."""
 
 import logging
 import pathlib
@@ -6,9 +6,12 @@ import sys
 
 import click
 import ni_measurement_plugin_sdk_service as nims 
-from client_session.session_constructor import (
+from device_communication_client.session_constructor import (
     DeviceCommunicationSessionConstructor,  # type: ignore[import-untyped]
-    INSTRUMENT_TYPE, #
+    INSTRUMENT_TYPE,
+)
+from device_comm_proto_stubs.device_comm_service_pb2 import (
+    Protocol,  # type: ignore[import-untyped]
 )
 
 script_or_exe = sys.executable if getattr(sys, "frozen", False) else __file__
@@ -17,43 +20,46 @@ measurement_service = nims.MeasurementService(
     service_config_path=service_directory / "SimpleMeasurement.serviceconfig",
     ui_file_paths=[service_directory / "SimpleMeasurement.measui"],
 )
+REGISTER_MAP_PATH = "" # Fill with actual register map path.
+REGISTER_NAME = "CAL_RX0" # Fill with actual register name.
 
 
 @measurement_service.register_measurement
 @measurement_service.configuration("Array in", nims.DataType.DoubleArray1D, [0.0])
 @measurement_service.configuration(
-    "device_comm_pin",
+    "resource_name",
     nims.DataType.IOResource,
-    "DUT_01",
+    "CustomInstrument",
     instrument_type=INSTRUMENT_TYPE,
 )
 @measurement_service.output("Array out", nims.DataType.DoubleArray1D)
-def measure(array_input, device_comm_pin: str) -> tuple[nims.DataType.DoubleArray1D]:
-    """TODO: replace the following line with your own measurement logic."""
+def measure(array_input, resource_name: str) -> tuple[nims.DataType.DoubleArray1D]:
+    """Initiate a measurement, ensuring necessary device communication to wake the device."""
     array_output = array_input
-    with measurement_service.context.reserve_session(device_comm_pin) as device_session_reservation:
-        logging.info("Initializing the device communication session...")
+    with measurement_service.context.reserve_session(resource_name) as device_session_reservation:
 
-    # Defaults to AUTO initialization behavior.
-        device_comm_session_constructor = DeviceCommunicationSessionConstructor(register_map_path="c:/Users/Public/Documents/National Instruments/Semi Device Control (64-bit)/Examples/Create register map using CSV/Csv source files/ProductName_RegisterMap.csv", reset=True, protocol="SPI")
+        # Defaults to AUTO initialization behavior.
+        device_comm_session_constructor = DeviceCommunicationSessionConstructor(
+            register_map_path=REGISTER_MAP_PATH,
+            reset=False,
+            protocol=Protocol.SPI,
+        )
         with device_session_reservation.initialize_session(
             device_comm_session_constructor, INSTRUMENT_TYPE
         ) as device_session_info:
+            logging.info("Initializing the device communication session...")
             device_session = device_session_info.session
+
+            # Powering the DUT is required to ensure the readiness.
+            # Performing read & write operations to ensure the device wake up.
             device_session.write_register(
                 register_name="CAL_RX1",
                 value="11111111",
             )
-            response = device_session.read_register(
+            device_session.read_register(
                 register_name="CAL_RX1",
             )
-            device_session.write_gpio_channel(
-                channel=1,
-                state=True,
-            )
 
-            print(response)
-    
     return (array_output,)
 
 
