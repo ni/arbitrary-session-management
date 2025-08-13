c5861343be0fd81a59b458c1643d3135a2181d5b
Giriharan Ravichandran <giriharan.ravichandran@ni.com>
2025-08-13 21:21:32 +0530

    fix: update measurement from string array to string control and indicator

--- src/custom_instr_session_sharing/examples/simple_measurement/measurement.py
+++ src/custom_instr_session_sharing/examples/simple_measurement/measurement.py
@@ -25,17 +25,17 @@ REGISTER_NAME = "CAL_RX0" # Fill with actual register name.
 
 
 @measurement_service.register_measurement
-@measurement_service.configuration("Register In", nims.DataType.StringArray1D, ["11111111"])
+@measurement_service.configuration("Register Value In", nims.DataType.String, "11111111")
 @measurement_service.configuration(
     "Resource name",
     nims.DataType.IOResource,
     "CustomInstrument",
     instrument_type=INSTRUMENT_TYPE,
 )
-@measurement_service.output("Register Out", nims.DataType.StringArray1D)
-def measure(register_in: str, resource_name: str) -> nims.DataType.StringArray1D:
+@measurement_service.output("Register Value Out", nims.DataType.String)
+def measure(register_value_in: str, resource_name: str) -> nims.DataType.String:
     """Initiate a measurement, ensuring necessary device communication to wake the device."""
-    register_out = []
+    register_value_out = ""
     with measurement_service.context.reserve_session(resource_name) as device_session_reservation:
 
         # Defaults to AUTO initialization behavior.
@@ -49,14 +49,12 @@ def measure(register_in: str, resource_name: str) -> nims.DataType.StringArray1D
         ) as device_session_info:
             logging.info("Initializing the device communication session...")
             device_session = device_session_info.session
+            # Ensure the device is powered on and ready for communication.
+            # Performing read & write operations to ensure the device wake up.
+            device_session.write_register(register_name=REGISTER_NAME,value=register_value_in)
+            register_value_out = device_session.read_register(register_name=REGISTER_NAME)
 
-            for register_value in register_in:
-                # Ensure the device is powered on and ready for communication.
-                # Performing read & write operations to ensure the device wake up.
-                device_session.write_register(register_name=REGISTER_NAME,value=register_value)
-                register_out.append(device_session.read_register(register_name=REGISTER_NAME))
-
-    return (register_out,)
+    return (register_value_out,)
 
 
 @click.command
